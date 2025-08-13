46cd2faac4f60ad1d571072a60800efad60f731a
LamBui <lamhy987@gmail.com>
2025-08-13 16:38:43 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Liễu N.Yên';
+const giftUrl = 'download.jpg';
+const eventName = 'Ah tặng e nà :3';
+const titleCard = 'Tặng người con gái ah thương';
+const contentCard = 'Hì tấm thiệp này thay cho lời chào của ah. Ah xin lỗi nha, có vẻ như ah xuất hiện vào lúc mà tâm trí em còn đag rối bời bởi ... nhiều thứ :)) ah biết hoặc đoán z, nhưng mà... ah vẫn phải nói, anh thích em :3 thích lúc nào thì ah không biết, tại nó đến tự nhiên quá, nhưng chắc cảm xúc này thì đơn phương thôi, tại em cũng có nhiều ng để ý ngó ngàng j đến một người như ah đâu nhỉ // vậy nha, những điều cần nói ah đã nói, nếu như em cho ah bước vào "thế giới" của em thì .. :)) ah kh bít, còn ko, thì hãy coi ah như mấy thg con trai lm phiền em và xóa kb với anh nha, tại ah không muốn tận mắt chứng kiến ng mình thích với ai khác đâu. Ah k đc như bao ng con trai khác ss chiều chuộng e, cho e cái e muốn .. ah chỉ bít iu và nếu có thể thì thay đổi cùng em thoi... cũng dài rồi, cho a câu trl nha đau tý ah chịu đc :)) tại từ trc tới giờ vẫn z mà kk FROM Lâm Bùi TO Liễu N.Yên ❣️';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+
+const giftImage = 'lieunhuyen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
